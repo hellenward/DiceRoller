b442b0961748a0cd4ef2498c6fab9e565e91b22f
Hellen Ward <hellenwellsward@gmail.com>
2019-04-05 07:33:37 +0100

    Now stores the result of each throw into an array, need to print onto screen next

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ $(document).ready(function() {
   $('.submitButton').click(function(diceNumber, sidesNumber) {
     var diceNumberVal = $('#diceNumber').val();
     var sidesNumberVal = $('#sidesNumber').val();
-    console.log(diceNumberVal);
-    console.log(sidesNumberVal);
+    const throwArray = []
+
+    if ($.isNumeric(diceNumberVal) && ($.isNumeric(sidesNumberVal)) ) {
+        for (let i = 0; i < diceNumberVal; i++) {
+          const result = Math.floor(Math.random() * sidesNumberVal + 1);
+          throwArray.push(result);
+        }
+    } else {
+      console.log("Oops, this doesn't look like a number!  Try again.");
+    };
+
+
   });
   $('.resetButton').click(function() {
     console.log("Goodbye then");
