2969c56a49ed05f1d1ea9a0b984c8a30a46e5ad4
Hellen Ward <hellenwellsward@gmail.com>
2019-04-12 06:37:41 +0100

    Put back as it was, might rewrite from scratch to handles different types of dice at once

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 
 $(document).ready(function() {
 
-  let throwsNumber = 0;
-
-  $('.addMore').click(function() {
-    const moreDice = document.querySelector(".moreDice");
-    moreDice.insertAdjacentHTML('afterend', `<label class="formText moreDice">Number of Dice rolled</label>
-    <input class="formText diceNumber1" type="text" placeholder="0"><label class="formText moreDice">Number of
-    Sides on Dice</label><input class="formText moreDice" class="sidesNumber1" type="text" placeholder="0">`);
-    throwsNumber ++;
-  })
+  const diceNumber = 0;
+  const sidesNumber = 0;
 
   $('.submitButton').click(function(diceNumber, sidesNumber) {
     document.getElementById("bitsToClear").innerHTML = '<div><p class="scoreThisRoll"></p></div><div><p class="totalScore"></p></div>';
+    var diceNumberVal = $('#diceNumber').val();
+    var sidesNumberVal = $('#sidesNumber').val();
     let throwArray = [];
-    $('.oneThrow').each(function() {
-      var diceNumberVal = $('.diceNumber').val();
-      var sidesNumberVal = $('.sidesNumber').val();
-      throwArray.push(`{throw = {"diceNumber": {diceNumberVal}, "sidesNumberVal": {sidesNumberVal}}`);
-    })
-
     $(".scoreTable").css({"visibility": "visible"});
 
 
     if ($.isNumeric(diceNumberVal) && ($.isNumeric(sidesNumberVal)) ) {
         for (let i = 0; i < diceNumberVal; i++) {
-          const result = Math.floor(Math.random() * sidesNumberVal + 1);
+          let result = Math.floor(Math.random() * sidesNumberVal + 1);
+          if(sidesNumberVal === "20" && result === 20) {
+            result = "green"
+          } else if (sidesNumberVal === "20" && result === 1) {
+            result = "red";
+          }
           throwArray.push(result);
         }
     } else {
@@ -34,10 +28,23 @@ $(document).ready(function() {
     };
 
     function throwHandler(throwArray) {
+      const placeHolder = document.querySelector(".scoreThisRoll");
       for(let i = 0; i < throwArray.length; i++) {
-        const placeHolder = document.querySelector(".scoreThisRoll");
-        placeHolder.insertAdjacentHTML('afterend', `<p class="newThrow">You threw a ${throwArray[i]}</p>`);
+        if (throwArray[i] === "green") {
+          placeHolder.insertAdjacentHTML('afterend', `<p class="newThrowGreen">You threw a 20</p>`);
+        } else if (throwArray[i] === "red") {
+          placeHolder.insertAdjacentHTML('afterend', `<p class="newThrowRed">You threw a 1</p>`);
+        } else {
+          placeHolder.insertAdjacentHTML('afterend', `<p class="newThrow">You threw a ${throwArray[i]}</p>`);
+        }
       };
+      $.each(throwArray, function(i) {
+        if (throwArray[i] === "green") {
+          throwArray[i] = 20;
+        } else if (throwArray[i] === "red") {
+          throwArray[i] = 1;
+        }
+      })
     }
 
     throwHandler(throwArray);
